86a8878a765a42e3856b8978e8910d2f32a102f5
Alfredo EM <alfredo.espiritu.m@gmail.com>
2016-06-24 17:51:53 -0500

    Refactoring new Vue

--- resources/assets/js/main/index.js
+++ resources/assets/js/main/index.js
@@ -1,96 +1,189 @@
-//$(".button-collapse").sideNav();
-
 var Todo = require('../components/Todo');
 
-$(document).on('click', '.task-create', function(e){
-    e.preventDefault();
-    console.log('create');
-    $('.task-create').hide();
-    Todo.create();
-});
-
-$(document).on('click', '.task-create-cancel', function(e){
-    e.preventDefault();
-    console.log('task-create-cancel');
-    Todo.createCancel();
-});
-
-$(document).on('click', '.task-store', function(e){
-    e.preventDefault();
-    console.log('create');
-    Todo.store();
-});
-
-$(document).on("keypress", "textarea", function(e) {
-
-    if (e.keyCode == 13) {
-        e.preventDefault();
-        let id = $(this).data('task-id');
-
-        if ($('#task_0').length) {
-            Todo.store();
-        } else {
-            Todo.update(id);
-        }
-
-        return false;
-    } else if (e.keyCode == 27) {
-        let id = $(this).data('task-id');
-        if (id == 0) {
-            Todo.createCancel();
-        } else {
-            Todo.editCancel(id);
-        }
-    }
-
-});
-
-$(document).on('click', '.task-edit', function(e){
-    e.preventDefault();
-    console.log('edit');
-    let id = $(this).data('task-id');
-    Todo.edit(id);
-});
-
-$(document).on('click', '.task-edit-cancel', function(e){
-    e.preventDefault();
-    console.log('edit-cancel');
-    let id = $(this).data('task-id');
-    Todo.editCancel(id);
-});
-
-$(document).on('click', '.task-update', function(e){
-    e.preventDefault();
-    console.log('update');
-    let id = $(this).data('task-id');
-    Todo.update(id);
-});
-
-$(document).on('click', '.task-delete-dialog', function(e){
-    e.preventDefault();
-    console.log('delete-dialog');
-    let id = $(this).data('task-id');
-    Todo.deleteDialog(id);
-});
-
-$(document).on('click', '.task-delete-cancel', function(e){
-    e.preventDefault();
-    Todo.deleteCancel();
-});
-
-$(document).on('click', '.task-delete', function(e){
-    e.preventDefault();
-    console.log('delete');
-    Todo.delete();
-});
-
 $('main').on('click', '.pagination a', function (e) {
+    e.preventDefault();
     $('.pagination').find('li').removeClass('active disabled');
     $(this).parent().addClass('active');
-    Todo.all($(this).attr('href').split('page=')[1]);
-    e.preventDefault();
+    vm.paginate($(this).attr('href').split('page=')[1]);
 });
 
-$(document).ready(function(){
-    Todo.all(1);
+var vm = new Vue({
+    el: 'main',
+    data: {
+        title: 'TODO Vue.js',
+        todos: [],
+        loading: true,
+        taskDialog: { id: 0, task: ''},
+        showCreate: true,
+        showCancel: false
+    },
+    ready: function () {
+        let self = this;
+
+        let request = $.ajax({url : 'todo?page=1', dataType: 'json'});
+        request.done(function(res){
+            self.loading = false;
+            self.renderTodos(res);
+        });
+    },
+    methods: {
+        paginate: function(page) {
+            this.createCancel();
+            
+            let container = $('#pagination-container');
+            container.fadeTo('slow', .6);
+            container.append('<div id="container-disabled" class="overlay"></div>');
+
+            let request = $.ajax({url : 'todo?page=' + page, dataType: 'json'});
+            let self = this;
+            request.done(function (res) {
+                container.fadeTo('slow', 1);
+                container.find('#container-disabled').remove();
+                self.renderTodos(res);
+            }).fail(function () {
+                alert('Posts could not be loaded.');
+            });
+        },
+
+        create: function(e) {
+            e.preventDefault();
+            this.showCreate = false;
+            this.showCancel = true;
+
+            $('#todo-list').find('li:eq(0)').append(this.templateCrate());
+            this.$compile(this.$el);
+
+            $('#task_0').find('#task_input_0').focus();
+        },
+
+        createCancel: function() {
+            this.showCancel = false;
+            this.showCreate = true;
+            $('#task_0').remove();
+        },
+
+        store: function() {
+
+            let input = $('#task_input_0');
+
+            if (input.val().trim()) {
+
+                let form_data = $('#task_form_0').serialize();
+                let url = (this.getActivePage()) ? `todo?page=${this.getActivePage()}` : 'todo';
+                let request = $.ajax({url: url, type: 'post', data: form_data});
+                var self = this;
+
+                request.done(function(res){
+                    $('#task_0').remove();
+                    self.showCancel = false;
+                    self.showCreate = true;
+                    self.renderTodos(res);
+                });
+            } else {
+                input.addClass('invalid').focus();
+            }
+        },
+
+        edit: function(id, e) {
+            e.preventDefault();
+            $('#view_' + id).hide();
+            $('#edit_' + id).show();
+            $(`#task_input_${id}`).select();
+        },
+        editCancel: function(id, e) {
+            e.preventDefault();
+            let text = $('#task_span_' + id).html();
+            $(`#task_input_${id}`).val(text.trim());
+            $('#edit_' + id).hide();
+            $('#view_' + id).show();
+        },
+        update: function(id, index, e) {
+            e.preventDefault();
+
+            let form = $(`#task_form_${id}`);
+            let request = $.ajax({url: 'todo/' + id, type: 'patch', data: form.serialize()});
+            let self = this;
+
+            request.done(function(res){
+                let task =  self.todos[index];
+                task.id = res.id;
+                task.task = res.task;
+                task.createdAt = res.createdAt;
+                task.UpdatedAt = res.UpdatedAt;
+                $('#view_' + id).show();
+                $('#edit_' + id).hide();
+            });
+        },
+        deleteDialog: function(id, e) {
+            e.preventDefault();
+
+            let task = $(`#task_span_${id}`);
+
+            $(`#task_${id}`).addClass('red accent-1');
+
+            this.taskDialog.id = id;
+            this.taskDialog.task = task.html().trim();
+            $('#modal-task-delete').openModal();
+        },
+        deleteCancel: function(e) {
+            e.preventDefault();
+            let modal = $('#modal-task-delete');
+            $(`#task_${this.taskDialog.id}`).removeClass('red accent-1');
+            modal.closeModal();
+        },
+        delete: function(e) {
+            e.preventDefault();
+            let modal = $('#modal-task-delete');
+            let page = this.getActivePage();
+            let id = this.taskDialog.id;
+            let url = (page) ? `todo/${id}?page=${id}` : `todo/${id}`;
+            let request = $.ajax({url: url, type: 'delete', data: {_token: $('#_token').val()}});
+            var self = this;
+
+            request.done(function(res){
+                modal.closeModal();
+                self.renderTodos(res);
+            });
+        },
+
+        renderTodos: function(res) {
+            this.todos = res.data;
+            $('.task-delete-dialog').leanModal({dismissible: false});
+            Todo.renderPagination(res);
+        },
+
+        getActivePage: function() {
+            let container = $('.pagination');
+            let active = container.find('li.active');
+            return (container.length && active.length) ? active.find('span').html().trim() : null;
+        },
+        
+        templateCrate: function() {
+            let token = $('#_token').val();
+
+            return `<li id="task_0" class="collection-item">                    
+                    <div id="edit_0" class="edit-mode">
+                        <form id="task_form_0" class="col s10">
+                            <input type="hidden" name="_token" value="${token}">
+                            <div class="row valign-wrapper">
+                                <div class="input-field col s10">
+                                    <textarea id="task_input_0" name="task" 
+                                    class="materialize-textarea validate" maxlength="255" 
+                                     v-on:keyup.enter="store" 
+                                     v-on:keyup.esc="createCancel"></textarea>
+                                    <label for="task_input_0">Task Description</label>
+                                </div>
+                                <div class="col s2 valign">
+                                    <a href="javascript:void(0)" class="secondary-content task-store"
+                                      v-on:click="store">
+                                        <i class="medium material-icons">done</i>
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                        
+                    </div>
+                </li>`;
+        }
+    }
 });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,58 @@
 
     <main>
         <ul id="todo-list" class="collection with-header">
-            <li class="collection-header"><h4 class="red-text">TODO VueJS</h4></li>
-            <li><div class="loader">Loading...</div></li>
+            <li class="collection-header"><h4 class="red-text">@{{ title }}</h4></li>
+            <li v-show="loading"><div class="loader">Loading...</div></li>
+
+            <li v-for="todo in todos" id="task_@{{ todo.id }}" class="collection-item">
+                <div id="view_@{{todo.id}}" class="view-mode row m0">
+                    <div class="text-justify col s12">
+                        <span class="break-word" id="task_span_@{{todo.id}}">
+                            @{{ todo.task }}</span>
+                    </div>
+                    <div class="right-align col s12">
+                        <a href="#modal-task-delete" class="secondary-content
+                            red-text text-accent-2" data-task-id="@{{todo.id}}"
+                           title="Delete Task" v-on:click="deleteDialog(todo.id, $event)">
+                            <i class="material-icons">delete</i>
+                        </a>
+
+                        <a href="javascript:void(0)" class="secondary-content task-edit"
+                           data-task-id="@{{todo.id}}" v-on:click="edit(todo.id, $event)">
+                            <i class="material-icons">mode_edit</i>
+                        </a>
+                    </div>
+                </div>
+
+                <div id="edit_@{{todo.id}}" class="edit-mode" style="display: none">
+                    <form id="task_form_@{{todo.id}}" class="col s10">
+                        <input type="hidden"
+                               name="_token" value="{{csrf_token()}}">
+                        <div class="row valign-wrapper">
+                            <div class="input-field col s10">
+                                   <textarea id="task_input_@{{todo.id}}" name="task"
+                                     class="materialize-textarea validate" data-task-id="@{{todo.id}}"
+                                     maxlength="255" v:on:keyup:esc="editCancel(todo.id, $event)"
+                                     v-on:keyup.enter="update(todo.id, $index, $event)">@{{ todo.task }}</textarea>
+                            </div>
+                            <div class="col s2 valign">
+                                <a href="javascript:void(0)" class="secondary-content
+                                   red-text text-accent-2" data-task-id="@{{todo.id}}"
+                                   v-on:click="editCancel(todo.id, $event)" >
+                                    <i class="material-icons font-35">not_interested</i>
+                                </a>
+
+                                <a href="javascript:void(0)" class="secondary-content"
+                                   v-on:click="update(todo.id, $index, $event)">
+                                    <i class="material-icons font-35">done</i>
+                                </a>
+                            </div>
+                        </div>
+
+                    </form>
+
+                </div>
+            </li>
         </ul>
 
         <div id="pagination-container"></div>
@@ -12,25 +62,32 @@
         <input type="hidden" id="_token" value="{{csrf_token()}}">
 
         <!-- Modal Structure -->
-        <div id="modal-task-delete" class="modal" data-taget-id="0">
+        <div id="modal-task-delete" class="modal">
             <div class="modal-content">
                 <h4>Are sure want to delete record?</h4>
-                <p id="task-to-delete-text">---</p>
+                <p id="task-to-delete-text">@{{ taskDialog.task }}</p>
             </div>
             <div class="modal-footer">
-                <a href="javascript:void(0)" class="waves-effect waves-red btn-flat task-delete-cancel">Cancel</a>
-                <a href="javascript:void(0)" class="waves-effect waves-green btn-flat task-delete">Delete</a>
+                <a href="javascript:void(0)" v-on:click="deleteCancel" class="waves-effect waves-red btn-flat">Cancel</a>
+                <a href="javascript:void(0)" class="waves-effect waves-green btn-flat task-delete"
+                v-on:click="delete">Delete</a>
             </div>
+        </div>
+
+        <div class="fixed-action-btn">
+            <div class="row">
+                <a href="javascript:void(0)" class="btn-floating btn-large right waves-effect
+            waves-light red task-create"
+                   v-on:click="create" v-show="showCreate"><i class="material-icons">add</i></a>
+
+                <a href="javascript:void(0)" class="btn-floating btn-large right waves-effect
+            waves-light red task-create-cancel"
+                   v-on:click="createCancel" v-show="showCancel"><i class="material-icons">not_interested</i></a>
+            </div>
+
         </div>
 
     </main>
 
-    <div class="fixed-action-btn">
-        <div class="row">
-            <a class="btn-floating btn-large right waves-effect waves-light red task-create"><i class="material-icons">add</i></a>
 
-            <a class="btn-floating btn-large right waves-effect waves-light red task-create-cancel" style="display: none"><i class="material-icons">not_interested</i></a>
-        </div>
-
-    </div>
 @endsection
